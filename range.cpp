fb7fd977c295368fc82f9321bac808158b0b6e7a
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-12 08:40:17 -0700

    Create range.cpp

---
+++ range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"Enter the number : "<<endl;
+cin>>num;
+if((num>0)&&(num<=10))
+cout<<"Yes(number is in the given range) "<<endl;
+else
+cout<<"Invalid range"<<endl;
+return 0;
+}
