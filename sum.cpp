7175f82af649ca45e721ab96a75af4a0db2be772
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-12 09:49:18 -0700

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n1,n2;
+cout<<"Enter the number1 : "<<endl;
+cin>>n1;
+cout<<"Enter the number2 : "<<endl;
+cin>>n2;
+cout<<"The Sum of two Number is : "<<n1+n2<<endl;
+return 0;
+}
