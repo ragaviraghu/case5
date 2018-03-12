7facf7cce99c8e6250e1f48427672c284195a3f1
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-12 09:17:01 -0700

    Create largesmall.cpp

---
+++ largesmall.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n,i,arr[n],small,large;
+cout<<"Enter a number"<<endl;
+cin>>n;
+cout<<"Enter the array elements : "<<endl;
+for(int i=0;i<n;i++)
+{cin>>arr[i];
+}
+small=arr[0];
+large=arr[0];
+for(i=1;i<n;i++)
+{
+if(arr[i]<small)
+small=arr[i];
+if(arr[i]>large)
+large=arr[i];
+}
+cout<<"Largest element is : "<<large<<endl;
+cout<<"Smallest element is : "<<small<<endl;
+return 0;
+}
